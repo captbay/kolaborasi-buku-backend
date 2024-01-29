a7b75736e673981c4d662383342b7bdd6a3e8e82
I Putu Agestya Pramana <agespramana9@gmail.com>
2024-01-29 10:38:41 +0700

    FIX bukudijual_penulis_pivot to id bcs cant make new items with uuid in filament

--- database/migrations/2024_01_23_074221_create_bukudijual_penulis_pivots_table.php
+++ database/migrations/2024_01_23_074221_create_bukudijual_penulis_pivots_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('bukudijual_penulis_pivot', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->id();
             $table->foreignUuid('buku_dijual_id')->constrained('buku_dijual')->cascadeOnDelete();
             $table->foreignUuid('penulis_id')->constrained('penulis')->cascadeOnDelete();
             $table->timestamps();
