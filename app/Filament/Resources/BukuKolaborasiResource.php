88ddebe8553f32f5787e95c761631601a1b568f9
I Putu Agestya Pramana <agespramana9@gmail.com>
2024-01-31 11:54:17 +0700

    FIX icon and make dashboard page filament

--- app/Filament/Resources/BukuKolaborasiResource.php
+++ app/Filament/Resources/BukuKolaborasiResource.php
@@ -36,7 +36,7 @@ class BukuKolaborasiResource extends Resource
 
     // protected static ?int $navigationSort = 3;
 
-    protected static ?string $navigationIcon = 'heroicon-s-book-open';
+    protected static ?string $navigationIcon = 'heroicon-s-folder-open';
 
     // navigation groups
 
