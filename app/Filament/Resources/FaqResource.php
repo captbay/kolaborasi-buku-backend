ffeb8326aee5c3d42ba550b26efbd0bfd7225cb6
I Putu Agestya Pramana <agespramana9@gmail.com>
2024-02-06 14:35:09 +0700

    new FEAT FAQ Resaurce Filament

---
+++ app/Filament/Resources/FaqResource.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FaqResource\Pages;
+use App\Filament\Resources\FaqResource\RelationManagers;
+use App\Models\Faq;
+use App\Models\konten_faq;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class FaqResource extends Resource
+{
+    protected static ?string $model = konten_faq::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-question-mark-circle';
+
+    protected static ?string $navigationLabel = 'FAQ';
+
+    protected static ?string $label = 'FAQ';
+
+    protected static ?string $slug = 'faq';
+
+    protected static ?string $title = 'FAQ';
+
+    protected static ?int $navigationSort = 5;
+
+    // navigation groups
+
+    protected static ?string $navigationGroup = 'Setting Web Customer';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('judul')
+                    ->required()
+                    ->columnSpan('full')
+                    ->maxLength(255)
+                    ->unique(konten_faq::class, 'judul', ignoreRecord: true),
+
+                Forms\Components\MarkdownEditor::make('answer')
+                    ->columnSpan('full')
+                    ->required(),
+
+                Forms\Components\Section::make('Status')
+                    ->schema([
+                        Forms\Components\Toggle::make('active_flag')
+                            ->label('Dipublish atau tidak')
+                            ->helperText('Jika tidak aktif maka buku ini akan disembunyikan pada tampilan penjualan buku')
+                            ->default(0),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('judul')
+                    ->wrap()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('answer')
+                    ->wrap()
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('active_flag')
+                    ->label('Dipublish')
+                    ->boolean()
+                    ->summarize(Tables\Columns\Summarizers\Count::make()->icons()),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])->defaultSort('created_at', 'desc')
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make()->slideOver(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])->iconButton()
+            ])
+            ->recordUrl(false)
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFaqs::route('/'),
+            'create' => Pages\CreateFaq::route('/create'),
+            'edit' => Pages\EditFaq::route('/{record}/edit'),
+        ];
+    }
+}
