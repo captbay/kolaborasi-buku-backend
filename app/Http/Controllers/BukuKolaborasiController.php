5f6d60ff5c13f23e01bf607bb52772d964fb5f71
I Putu Agestya Pramana <agespramana9@gmail.com>
2024-03-19 16:00:02 +0700

    Feat: add api show detail buku

--- app/Http/Controllers/BukuKolaborasiController.php
+++ app/Http/Controllers/BukuKolaborasiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\buku_kolaborasi;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class BukuKolaborasiController extends Controller
@@ -15,7 +16,8 @@ class BukuKolaborasiController extends Controller
         try {
             // get the most count buku_dijual in list_transasksi_buku
             $data = buku_kolaborasi::with('kategori')
-                ->where('active_flag', '1');
+                ->where('active_flag', '1')
+                ->where('dijual', 0);
 
             // filter by kategori
             if ($request->has("kategori")) {
@@ -95,8 +97,158 @@ class BukuKolaborasiController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(buku_kolaborasi $buku_kolaborasi)
+    public function show(Request $req)
     {
-        //
+        try {
+            $data = buku_kolaborasi::with('kategori')
+                ->with(['bab_buku_kolaborasi' => function ($query) {
+                    $query->with(['user_bab_buku_kolaborasi'
+                    => function ($query) {
+                        $query->orderBy('created_at', 'desc');
+                    }])
+                        ->where('active_flag', 1)
+                        ->orderBy('created_at', 'desc');
+                }])
+                ->where('slug', $req->slug)
+                ->where('active_flag', 1)
+                ->where('dijual', 0)
+                ->first();
+
+            // set temp count for timeline
+            $count_kontributor = 0;
+            $count_upload = 0;
+            $count_editing = 0;
+            $count_selesai = 0;
+
+            // bab buku
+            $bab_buku = $data->bab_buku_kolaborasi->map(function ($item)
+            use (
+                &$count_kontributor,
+                &$count_upload,
+                &$count_editing,
+                &$count_selesai,
+            ) {
+                // check if user_bab_buku_kolaborasi is exist
+                if ($item->user_bab_buku_kolaborasi->first()) {
+                    // compere datetime_deadline to get is_terjual true or false
+                    if ($item->user_bab_buku_kolaborasi->first()->datetime_deadline > Carbon::now()) {
+                        $terjual = true;
+                        $count_kontributor++;
+                    } else {
+                        $terjual = false;
+                    }
+
+                    // check if status is UPLOADED
+                    if ($item->user_bab_buku_kolaborasi->first()->status == "UPLOADED") {
+                        $count_upload++;
+                    }
+
+                    // check if status is REVISI
+                    if ($item->user_bab_buku_kolaborasi->first()->status == "REVISI") {
+                        $count_editing++;
+                    }
+
+                    // check if status is DONE
+                    if ($item->user_bab_buku_kolaborasi->first()->status == "DONE") {
+                        $count_selesai++;
+                    }
+                } else {
+                    $terjual = false;
+                }
+
+                return [
+                    'id' => $item->id,
+                    'no_bab' => $item->no_bab,
+                    'judul' => $item->judul,
+                    'harga' => $item->harga,
+                    'durasi_pembuatan' => $item->durasi_pembuatan,
+                    'deskripsi' => $item->deskripsi,
+                    'is_terjual' => $terjual,
+                ];
+            });
+
+            // set status of kolaborasi
+            if ($count_kontributor >= $data->jumlah_bab) {
+                $status_kolaborasi = "closed";
+            } else {
+                $status_kolaborasi = "open";
+            }
+
+            // set data for timeline kolaborasi
+            // Kontributor
+            // Upload Naskah
+            // Editing Oleh Editor
+            // Naskah Selesai
+            // Input ISBN
+            // Buku Publish
+            $timeline_kolaborasi = [
+                [
+                    'id' => 1,
+                    'judul' => 'Kontributor',
+                    'count' => $count_kontributor . '/' . $data->jumlah_bab,
+                    'status' => $count_kontributor >= $data->jumlah_bab ? 'selesai' : 'menunggu'
+                ],
+                [
+                    'id' => 2,
+                    'judul' => 'Upload Naskah',
+                    'count' => $count_upload . '/' . $data->jumlah_bab,
+                    'status' => $count_upload >= $data->jumlah_bab ? 'selesai' : 'menunggu'
+                ],
+                [
+                    'id' => 3,
+                    'judul' => 'Editing Oleh Editor',
+                    'count' => $count_editing . '/' . $data->jumlah_bab,
+                    'status' => $count_editing >= $data->jumlah_bab ? 'selesai' : 'menunggu'
+                ],
+                [
+                    'id' => 4,
+                    'judul' => 'Naskah Selesai',
+                    'count' => $count_selesai . '/' . $data->jumlah_bab,
+                    'status' => $count_selesai >= $data->jumlah_bab ? 'selesai' : 'menunggu'
+                ],
+                [
+                    'id' => 5,
+                    'judul' => 'Input ISBN',
+                    'status' => $count_selesai == $data->jumlah_bab ? 'proses' : 'menunggu'
+                ],
+                [
+                    'id' => 6,
+                    'judul' => 'Buku Publish',
+                    'status' => $data->dijual == 1 ? 'selesai' : 'menunggu'
+                ],
+            ];
+
+            // filter only needed data
+            $data = [
+                'slug' => $data->slug,
+                'judul' => $data->judul,
+                'deskripsi' => $data->deskripsi,
+                'kategori' => $data->kategori->nama,
+                'cover_buku' => $data->cover_buku,
+                'jumlah_bab' => $data->jumlah_bab,
+                'status_kolaborasi' => $status_kolaborasi,
+                'bab' => $bab_buku,
+                'timeline_kolaborasi' => $timeline_kolaborasi
+            ];
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'error',
+                'data' => $e->getMessage()
+            ], 500);
+        }
+
+        if (!$data) {
+            return response()->json([
+                'message' => 'error',
+                'data' => 'kolaborasi not found'
+            ], 404);
+        }
+
+        // return the resource
+        return response()->json([
+            'success' => true,
+            'message' => 'kolaborasi retrieved successfully.',
+            'data' => $data
+        ], 200);
     }
 }
