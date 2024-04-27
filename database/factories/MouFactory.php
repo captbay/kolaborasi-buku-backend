89cd67bc8a5873b9d653e1f529d6c2dc686749e2
I Putu Agestya Pramana <agespramana9@gmail.com>
2024-04-27 22:03:32 +0700

    Feat: change file name factory

--- database/factories/MouFactory.php
+++ database/factories/MouFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\konten_faq>
  */
-class MouFactory extends Factory
+class mouFactory extends Factory
 {
     /**
      * Define the model's default state.
--- database/factories/PenulisFactory.php
+++ database/factories/PenulisFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\penulis>
  */
-class PenulisFactory extends Factory
+class penulisFactory extends Factory
 {
     /**
      * Define the model's default state.
