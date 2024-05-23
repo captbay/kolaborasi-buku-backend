e2ce2bca8b2727c5a93ddf64550373f2560793ef
I Putu Agestya Pramana <agespramana9@gmail.com>
2024-05-23 16:43:15 +0700

    Feat: add default value DB config

--- database/migrations/2024_01_23_074727_create_config_webs_table.php
+++ database/migrations/2024_01_23_074727_create_config_webs_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -19,6 +20,36 @@ return new class extends Migration
             $table->boolean('active_flag');
             $table->timestamps();
         });
+
+        $data = [
+            [
+                'key' => 'no_rek',
+                'tipe' => 'TEXT',
+                'value' => 'NOMOR REKENING BANK',
+            ],
+            [
+                'key' => 'bank_rek',
+                'tipe' => 'TEXT',
+                'value' => 'NAMA BANK PENERBITAN',
+            ],
+            [
+                'key' => 'nama_rek',
+                'tipe' => 'TEXT',
+                'value' => 'NAMA REKENING BANK',
+            ],
+        ];
+
+        foreach ($data as $d) {
+            DB::table('config_web')->insert([
+                'id' => \Ramsey\Uuid\Uuid::uuid4(),
+                'key' => $d['key'],
+                'tipe' => $d['tipe'],
+                'value' => $d['value'],
+                'active_flag' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 
     /**
