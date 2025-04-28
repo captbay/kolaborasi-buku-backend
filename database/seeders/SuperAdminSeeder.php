02d846ee570bf4d5a76f6d14e0c524ceed0cb568
RayWP <rwp1byte@gmail.com>
2025-04-28 23:34:47 +0800

    fix superadminseeder

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Config;
 
 class SuperAdminSeeder extends Seeder
 {
@@ -14,13 +15,26 @@ class SuperAdminSeeder extends Seeder
      */
     public function run(): void
     {
+        $adminEmail = 'admin@penerbitarunika.com';
+        
         DB::table('users')->insert([
             'id' => \Ramsey\Uuid\Uuid::uuid4(),
-            'name' => 'Super Admin',
-            'email' => 'admin@admin.com',
+            'nama_depan' => 'Super',
+            'nama_belakang' => 'Admin',
+            'email' => $adminEmail,
             'password' => Hash::make('password'),
-            'role' => 'ADMIN',
+            'no_telepon' => '081234567890',
+            'tgl_lahir' => now()->subYears(25)->toDateString(),
+            'gender' => 'L',
+            'alamat' => 'Alamat Admin',
+            'provinsi' => 'DKI Jakarta',
+            'kecamatan' => 'Kebayoran Baru',
+            'kota' => 'Jakarta Selatan',
+            'kode_pos' => '12345',
+            'foto_profil' => null,
+            'status_verif_email' => true,
             'email_verified_at' => now(),
+            'role' => 'ADMIN',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
